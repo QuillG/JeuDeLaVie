7b843b1e8193a2df8041cfbeecab6f6ba54e3ca4
QuillG <quentin.guillemand@gmail.com>
2025-11-17 16:19:52 +0100

    add config electron

--- electron/main.js
+++ electron/main.js
@@ -1,21 +1,32 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1000,
+    width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  // En dev : charge le serveur Vite
-  win.loadURL('http://localhost:5173')
-  // En prod : charge le build final
-  // win.loadFile(path.join(__dirname, '../dist/index.html'))
+  // DEV MODE (Vite)
+  if (!app.isPackaged) {
+    win.loadURL("http://localhost:5173");
+  } 
+  // BUILD MODE (.exe installé)
+  else {
+    const indexPath = path.resolve(__dirname, "../dist/index.html");
+    win.loadURL(`file://${indexPath}`);
+  }
 }
 
-app.whenReady().then(() => {
-  createWindow()
-})
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
